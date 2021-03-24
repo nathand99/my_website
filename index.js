d514630ea9b738c310a92843ebcebe2a83685dc4
nathand99 <z5204935@ad.unsw.edu.au>
2021-03-24 17:44:39 +1100

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express') // this is an include/import statement
+const app = express();
+const port = 8000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`)
+});
