9a7cf64bc09b9d715c385c115fc7747eba147fd6
nathand99 <z5204935@ad.unsw.edu.au>
2021-03-24 19:56:44 +1100

    changed folder structure, initiate project

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+  // could do:
+  // res.json() - send json
+  // res.sendFile()
+  //res.render()
+});
+
+module.exports = router;
+
+//Route parameters are named URL segments used to capture values at specific positions in the URL. 
+//The named segments are prefixed with a colon and then the name (e.g. /:your_parameter_name/
